3f960e86c7503ec9efa092eb215945acb6c8722c
thilaga <thilagab@hotmail.com>
2024-03-20 12:01:56 +1100

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Thilagawathy Duraisamy on 20/3/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
